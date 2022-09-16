ff7bb9b5cffd6b59030c5e8516f68ba52e01d6ad
littlexfish <i529561@gmail.com>
2022-09-16 22:15:01 +0800

    init commit

---
+++ user-side/SimpleOrderSystem/app/src/main/java/edu/nptu/dllab/sos/activity/TestActivity.kt
@@ -0,0 +1,12 @@
+package edu.nptu.dllab.sos.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import edu.nptu.dllab.sos.R
+
+class TestActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_test)
+	}
+}
